e48426b4b99612a528194317ad8e9cfbdc2301c8
senaarth <senaarth@gmail.com>
2021-08-22 21:52:09 -0300

    adds project description

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Shortest Path</h1>
+      <p style={{ width: '30%', minWidth: '300px', marginBottom: '5rem' }}>
+        O propósito da aplicação é demonstrar visualmente a atuação do algoritmo de 
+        Dijkstra num grafo direcionado com arestas ponderadas. O usuário pode escolher
+        a origem e o destino do caminho e após um duplo clique no fundo branco do 
+        container o programa irá destacar o trajeto mais otimizado dentre os possíveis.
+      </p>
     </div>
   );
 }
